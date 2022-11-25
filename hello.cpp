7d3ba79ea8727beef071983d2bfe9549862aacf0
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2022-11-25 12:03:11 +0100

    new file!

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+    int start = clock();
+    int count = 0;
+    for (int i = 0; i < 1000000000; i++) {
+        count++;
+    }
+    int end = clock();
+    cout << count;
+    cout << "done! it took: "<< ((float)end - start)/CLOCKS_PER_SEC << " seconds";
+}
